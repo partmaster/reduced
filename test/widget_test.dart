b09eebcb0543777a6e49855fd6767df78a14289c
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-01-24 17:18:25 +0100

    widget test fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,22 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:counter_app/app.dart';
+import 'package:counter_app/model.dart';
+import 'package:counter_app/util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    const state = MyAppState(
+      title: 'Flutter Demo Home Page',
+      counter: 0,
+    );
+    const app = MyApp();
+    const provider = StateProvider(state: state, child: app);
+    await tester.pumpWidget(provider);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
