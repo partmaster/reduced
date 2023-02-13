4ec91318e30067f195bc0acfd90d2da939fa9225
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-02-13 09:38:29 +0100

    solidart refactored

--- lib/binder.dart
+++ lib/binder.dart
@@ -1,12 +1,21 @@
 // binder.dart
 
-// export 'stateful/stateful_binder.dart';
-// export 'riverpod/riverpod_binder.dart';
 // export 'bloc/bloc_binder.dart';
-//  export 'redux/redux_binder.dart';
 // export 'getit/getit_binder.dart';
 // export 'getx/getx_binder.dart';
 // export 'mobx/mobx_binder.dart';
-export 'solidart/solidart_binder.dart';
 // export 'provider/provider_binder.dart';
+// export 'redux/redux_binder.dart';
+// export 'riverpod/riverpod_binder.dart';
+// export 'setstate/setstate_binder.dart';
+export 'solidart/solidart_binder.dart';
+
 // export 'mock/mock_binder.dart';
+
+/*
+binder
+fishredux
+fluttercommands
+statesrebuilder
+triplepattern
+*/
---
+++ lib/solidart/solidart.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_solidart/flutter_solidart.dart';
+// ignore: implementation_imports
+import 'package:solidart/src/core/signal_selector.dart';
+
+import '../reduceable.dart';
+
+extension ReduceableOnAppStateSignal<S> on Signal<S> {
+  S getState() => value;
+
+  void reduce(Reducer<S> reducer) => value = reducer(value);
+
+  Reduceable<S> get reduceable => Reduceable(getState, reduce, this);
+}
+
+Widget signalBuilder<S, P>(
+  Signal<S> signal,
+  P Function(Reduceable<S>) selector,
+  Widget Function({Key? key, required P props}) builder,
+) =>
+    SignalBuilder(
+      signal: SignalSelector<S, P>(
+        signal: signal,
+        selector: (_) => selector(signal.reduceable),
+        options: SignalOptions(comparator: (a, b) => a == b),
+      ),
+      builder: (_, value, ___) => builder(props: value),
+    );
