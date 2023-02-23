52f44779fe9da05484d69ccad407e194a5d242ce
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-02-23 16:55:20 +0100

    code formatted

--- lib/reduced_functions.dart
+++ lib/reduced_functions.dart
@@ -2,13 +2,16 @@ import 'package:flutter/widgets.dart' show Key, Widget;
 
 import 'reducible.dart';
 
-/// A function that transforms a Reducible to another type. 
-/// 
+/// A function that transforms a Reducible to another type.
+///
 /// The type parameter `S` is the type of the state of the [Reducible].
-/// The type parameter `P` is the return type of function.
+/// The type parameter `P` is the return type of the function.
 typedef ReducibleTransformer<S, P> = P Function(Reducible<S>);
 
 // A function that builds a Widget from a property parameter.
-
+///
 /// The type parameter `P` is the type of the parameter.
-typedef PropsWidgetBuilder<P> = Widget Function({Key? key, required P props});
+typedef PropsWidgetBuilder<P> = Widget Function({
+  Key? key,
+  required P props,
+});
