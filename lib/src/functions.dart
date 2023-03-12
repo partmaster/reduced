a2b27b8533404a3951907f4496df7ad329fa30ed
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-03-12 21:09:12 +0100

    define EventListener

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/widgets.dart' show Key, Widget;
 
+import 'event.dart';
 import 'store.dart';
 
 /// A function that transforms a ReducedStore to another type.
@@ -17,3 +18,11 @@ typedef ReducedWidgetBuilder<P> = Widget Function({
   Key? key,
   required P props,
 });
+
+/// A function that can be registered as an eventDispatched listener at a ReducedStore.
+///
+/// A registered listener is called by the ReducedStore at the end of each dispatch method execution.
+typedef EventListener<S> = void Function(
+  ReducedStore<S> store,
+  Event<S> event,
+);
