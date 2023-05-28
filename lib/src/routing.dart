dc9797c55144f711bc3872587d7c5e3787412299
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-05-28 17:52:40 +0200

    add nil routing context

--- lib/src/routing.dart
+++ lib/src/routing.dart
@@ -1,10 +1,16 @@
-/// interface for getting route data.
-abstract class RoutingState {
-  const RoutingState();
-  String? get name;
-  Map<String, String> get pathParameters;
-  Map<String, String> get queryParameters;
-  Map<String, List<String>> get queryParametersAll;
+/// class for getting route data.
+class RoutingState {
+  const RoutingState({
+    this.name,
+    this.pathParameters = const <String, String>{},
+    this.queryParameters = const <String, String>{},
+    this.queryParametersAll = const <String, List<String>>{},
+  });
+
+  final String? name;
+  final Map<String, String> pathParameters;
+  final Map<String, String> queryParameters;
+  final Map<String, List<String>> queryParametersAll;
 }
 
 /// interface for push and pop pages via routes.
@@ -27,7 +33,36 @@ abstract class RoutingStack {
 
 /// RoutingContext for page props mappers.
 class RoutingContext {
+  static const nil = RoutingContext(
+    RoutingState(),
+    NilRoutingStack.instance,
+  );
+
   const RoutingContext(this.state, this.stack);
   final RoutingState state;
   final RoutingStack stack;
+}
+
+class NilRoutingStack extends RoutingStack {
+  const NilRoutingStack._();
+  static const instance = NilRoutingStack._();
+  @override
+  void pop<T>([T? value]) {}
+
+  @override
+  Future<T?> pushNamed<T>(
+    String name, {
+    Map<String, String> pathParameters = const <String, String>{},
+    Map<String, dynamic> queryParameters = const <String, dynamic>{},
+    Object? extra,
+  }) =>
+      Future.value(null);
+
+  @override
+  void pushReplacementNamed<T>(
+    String name, {
+    Map<String, String> pathParameters = const <String, String>{},
+    Map<String, dynamic> queryParameters = const <String, dynamic>{},
+    Object? extra,
+  }) {}
 }
