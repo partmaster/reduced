e96ff0a5d32c566bd2a4dcb40b21de03c158e7cd
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-05-28 17:20:41 +0200

    add push parameters

--- lib/src/routing.dart
+++ lib/src/routing.dart
@@ -1,23 +1,33 @@
 /// interface for getting route data.
 abstract class RoutingState {
   const RoutingState();
-  String? get routeName;
+  String? get name;
   Map<String, String> get pathParameters;
   Map<String, String> get queryParameters;
   Map<String, List<String>> get queryParametersAll;
 }
 
-/// interface for dispatching routes.
-abstract class RoutingDispatcher {
-  const RoutingDispatcher();
-  Future<T?> pushNamed<T>(String routeName);
-  Future<T?> pushReplacementNamed<T>(String routeName);
+/// interface for push and pop pages via routes.
+abstract class RoutingStack {
+  const RoutingStack();
+  Future<T?> pushNamed<T>(
+    String name, {
+    Map<String, String> pathParameters = const <String, String>{},
+    Map<String, dynamic> queryParameters = const <String, dynamic>{},
+    Object? extra,
+  });
+  void pushReplacementNamed<T>(
+    String name, {
+    Map<String, String> pathParameters = const <String, String>{},
+    Map<String, dynamic> queryParameters = const <String, dynamic>{},
+    Object? extra,
+  });
   void pop<T>([T? value]);
 }
 
 /// RoutingContext for page props mappers.
 class RoutingContext {
-  const RoutingContext(this.state, this.dispatcher);
+  const RoutingContext(this.state, this.stack);
   final RoutingState state;
-  final RoutingDispatcher dispatcher;
+  final RoutingStack stack;
 }
